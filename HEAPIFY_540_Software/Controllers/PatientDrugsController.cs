b615207e1a7aedcc80c4c426187fa427ba4cf885
ERICA LINDSEY <LINDSEED@EMAIL.USCUPSTATE.EDU>
2017-11-30 14:33:22 -0500

    Updated menu and employee personal info pages

--- HEAPIFY_540_Software/Controllers/PatientDrugsController.cs
+++ HEAPIFY_540_Software/Controllers/PatientDrugsController.cs
@@ -10,7 +10,8 @@ using HEAPIFY_540_Software.Models;
 
 namespace HEAPIFY_540_Software.Controllers
 {
-    [Authorize(Roles = "Medical Staff")]
+    //[Authorize(Roles = "Medical Staff")]
+    [Authorize]
     public class PatientDrugsController : Controller
     {
         private HEAPIFY_540_SoftwareContext db = new HEAPIFY_540_SoftwareContext();
